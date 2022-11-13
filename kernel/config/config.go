e8e25d1720cf5da167419c1d07618b2f2449c280
siam <59419979@qq.com>
2022-11-13 12:37:08 +0800

    优化set.ini生成

--- kernel/config/config.go
+++ kernel/config/config.go
@@ -12,13 +12,20 @@ var DbName string
 var BatPath string
 var BatIdentify string
 
-var Cfg, _ = ini.Load("set.ini") //初始化一个cfg
+var Cfg *ini.File
 
 func IsDev() bool {
 	return Cfg.Section("").Key("app_mode").String() == "dev"
 }
 
 func Init() {
+	iniFileExists, _ := PathExists("set.ini")
+	if !iniFileExists {
+		createIni()
+	}
+
+	Cfg, _ = ini.Load("set.ini") //初始化一个cfg
+
 	DbPath = Cfg.Section("").Key("db_path").String()
 	fmt.Println(DbPath)
 	if DbPath == "" {
@@ -48,4 +55,31 @@ func Init() {
 	//fmt.Println(BatIdentify)
 	//os.Exit(1)
 
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+func createIni() {
+	file6, err := os.Create("set.ini")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data := "app_mode = dev\n\ndb_path = \ndb_name = \n\nbat_path = \n\nbat_identify = \n\n[gin]\naddress = 127.0.0.1\nport = 8899"
+
+	_, err = file6.WriteString(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_ = file6.Close()
 }
